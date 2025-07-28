ddcf29549c6fb504a772051b222cfbc49a6193e4
UlisesPlaRocher <ulisesplarocher@gmail.com>
2025-07-29 00:01:56 +0200

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,90 @@
-import Image from "next/image";
+"use client";
+
+import TableCent from "./tableCenter";
+import { tableData as initialData } from "./lib/data";
+import { useState } from "react";
+import { MoveRight, MoveLeft } from "lucide-react";
+
+type TableRow = {
+  id: number;
+  user: string;
+  email: string;
+};
 
 export default function Home() {
-  return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const [data, setData] = useState<TableRow[]>(initialData.slice(0, 10));
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  const [pag, setPag] = useState<number>(1);
+
+  return (
+    <div className="relative min-h-screen flex flex-col justify-center items-center">
+      <TableCent data={data} />
+
+      <div className="relative flex items-center justify-center w-[35rem]">
+        <div
+          className="
+    flex items-center justify-center
+    p-2 rounded-lg
+    mt-3
+    bg-gray-900
+    cursor-pointer
+    hover:bg-red-900
+    shadow-lg hover:shadow-xl
+    transition
+    select-none
+  "
+          onClick={() => {
+            setPag((prev) => {
+              if (prev === 1) return prev;
+              setData(initialData.slice((prev - 2) * 10, (prev - 1) * 10));
+              return prev - 1;
+            });
+          }}
+        >
+          <MoveLeft className="text-red-400" />
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+
+        <div
+          className="
+    absolute top-0 right-0
+    flex items-center justify-center
+    w-10 h-10     /* ancho y alto iguales para círculo */
+    rounded-full
+    mt-3
+    bg-gray-900
+    
+   
+    text-[#f5e942]
+    shadow-lg hover:shadow-xl
+    transition
+    select-none
+  "
         >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+          {pag}
+        </div>
+      </div>
+      <div
+        className="
+    inline-flex items-center justify-center
+    p-2 rounded-lg
+    mt-3
+    bg-gray-900
+    cursor-pointer
+    hover:bg-red-900
+    shadow-lg hover:shadow-xl
+    transition
+    select-none
+  "
+        onClick={() => {
+          setPag((prev) => {
+            if (prev === 2) return prev;
+            setData(initialData.slice(prev * 10, (prev + 1) * 10));
+            return prev + 1;
+          });
+        }}
+      >
+        <MoveRight className="text-red-400" />
+      </div>
     </div>
   );
 }
---
+++ app/tableCenter.tsx
@@ -0,0 +1,105 @@
+"use client";
+
+import { useRef } from "react";
+
+type TableRow = {
+  id: number;
+  user: string;
+  email: string;
+};
+
+interface TableCentProps {
+  data: TableRow[];
+}
+
+export default function TableCent({ data }: TableCentProps) {
+  const columnRefs = useRef(new Map<number, HTMLDivElement>());
+
+  const original = useRef<number | null>(null);
+
+  const col = useRef<HTMLDivElement | null>(null);
+
+  const currentWidth = useRef<number | null>(null);
+
+  if (!data.length) return <div>No data</div>;
+
+  const keys = Object.keys(data[0]) as (keyof TableRow)[];
+  const columnValues = keys.map((key) => data.map((row) => row[key]));
+
+  const handleMouseMove = (e: globalThis.MouseEvent) => {
+    const moveX = e.clientX - original.current!;
+
+    const newWidth = currentWidth.current! + moveX;
+
+    col.current!.style.width = `${newWidth}px`;
+  };
+
+  const handleMouseUp = () => {
+    window.removeEventListener("mouseup", handleMouseUp);
+    window.removeEventListener("mousemove", handleMouseMove);
+    document.body.style.userSelect = "";
+  };
+
+  const handleMouseDown = (index: number) => (e: React.MouseEvent) => {
+    document.body.style.userSelect = "none";
+
+    original.current = e.clientX;
+    col.current = columnRefs.current.get(index)!;
+
+    currentWidth.current = parseInt(
+      window.getComputedStyle(col.current!).width,
+      10
+    );
+
+    window.addEventListener("mousemove", handleMouseMove);
+    window.addEventListener("mouseup", handleMouseUp);
+  };
+
+  return (
+    <>
+      <div
+        className="flex max-w-[65%]  max-h-[70vh] min-h-[55vh]  border shadow-[0_0_20px_#4cc9f066] rounded"
+        style={{ scrollbarGutter: "stable" }}
+      >
+        {keys.map((key, index) => (
+          <div key={index} className="flex">
+            <div
+              className="flex flex-col p-4 pt-2 bg-custom-dark rounded shadow max-w-[20rem]  h-full"
+              ref={(el) => {
+                if (el) {
+                  columnRefs.current.set(index, el);
+                } else {
+                  columnRefs.current.delete(index);
+                }
+              }}
+            >
+              <div className="border-b-2 border-gray-400 pb-1 mb-4 flex justify-center font-bold capitalize">
+                {key}
+              </div>
+
+              {columnValues[index].map((value, jindex) => (
+                <div
+                  key={`${key}-${jindex}`}
+                  className="truncate overflow-hidden text-ellipsis whitespace-nowrap px-2 py-1 w-[10rem]"
+                  title={String(value)}
+                >
+                  {value || "\u00A0"}
+                </div>
+              ))}
+            </div>
+
+            {index < keys.length - 1 && (
+              <div
+                className="relative cursor-ew-resize flex items-center"
+                onMouseDown={handleMouseDown(index)}
+              >
+                <div className="absolute inset-y-0 left-1/2 -translate-x-1/2 w-6" />
+                <div className="w-[0.1rem] bg-gray-400 mx-auto z-10 h-[95%] pt-10" />
+              </div>
+            )}
+          </div>
+        ))}
+      </div>
+    </>
+  );
+}
